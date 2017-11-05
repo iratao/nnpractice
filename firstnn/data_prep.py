a20c5906389b3ea9aa3f75552f95373597bd3518
iratao <iratao@gmail.com>
2017-11-05 22:51:22 +0800

    First nn

---
+++ firstnn/data_prep.py
@@ -0,0 +1,13 @@
+# Visualize your data a bit and get a general feeling.
+
+# Process categorical features and convert to dummny variables
+
+# Drop useless columns(features)
+
+# Standardize(normalize) continuous features
+
+# Splitting the data into training, testing, and validation sets
+# 1. Save data for approximately the last 21 days 
+# 2. Now remove the test data from the data set 
+# 3. Separate the data into features and targets
+# 4. Hold out the last 60 days or so of the remaining data as a validation set
